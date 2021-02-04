2ed4bf70f38788bb8d4f171075b833e088b17ec8
jhosep98 <jhosep149@hotmail.com>
2021-02-04 09:08:03 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Blog post app
+
+> challenge blog post app.📱😎
+
+## Table of contents
+
+- [Technologies](#technologies)
+- [Contact](#contact)
+
+## Technologies
+
+- Html
+- Css
+- Javascript
+- Reactjs
+- Bootstrap
+
+## Contact
+
+Created by [@jhosep98](https://jhosep98.github.io/jdb-portfolio/) - feel free to contact me!
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src/index.jsx'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?(jsx|js)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+      {
+        enforce: 'pre',
+        test: /\.m?(jsx|js)$/,
+        exclude: /(node_modules)/,
+        loader: 'eslint-loader',
+        options: {
+          fix: true,
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './public/index.html',
+      filename: './index.html',
+    }),
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+};
